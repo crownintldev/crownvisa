7db521eaf768a9d80679c7ad5b72efde43531871
crown <crownintldev@gmail.com>
2023-12-08 17:33:35 +0500

    'ok'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import ContextProvider from "./ContextProvider";
 import QueryProvider from "./QueryProvider";
-import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -40,7 +39,6 @@ export default function RootLayout({
               theme="light"
             />
             {children}
-            <SpeedInsights />
             <ToastContainer />
           </ContextProvider>
         </QueryProvider>
